1d70a935a233175d5e872a0eaf52dcbd7440de1d
yacchin1205 <yacchin1205@users.noreply.github.com>
2022-05-20 06:10:02 +0000

    jekyll build from Action a749aa6b6436c98abe563335ad64be5baef13054

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "CoursewareHub利用事例 - 群馬大学 井上先生",
+        "excerpt":"CoursewareHubの利用事例を国立情報学研究所 学術基盤オープンフォーラム2021にて 群馬大学・数理データ科学教育研究センター 井上先生にCoursewareHubの利用事例をご紹介いただきました！   資料は以下のリンクからご参照いただけます。     ","categories": [],
+        "tags": ["利用例","群馬大学"],
+        "url": "/2021/08/10/gunma-u.html",
+        "teaser": null
+      },{
+        "title": "CoursewareHub構築手順Notebookを公開しました",
+        "excerpt":"Jupyter Notebookベースの「実行可能な」手順書形式による構築手順の配布を開始しました。   詳細は以下をご参照ください。   https://github.com/NII-cloud-operation/CoursewareHub-LC_deploy   本手順は、2022年度 情報処理技術セミナー(クラウド編)でもご紹介をする予定です: https://contents.nii.ac.jp/hrd/joho-karuizawa-cloud/2022/result   ","categories": [],
+        "tags": ["Notebook"],
+        "url": "/2022/05/20/deploy.html",
+        "teaser": null
+      }]
